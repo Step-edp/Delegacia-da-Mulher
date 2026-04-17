6a8576534fb65fbe66dca6ffa0b3813563e2c95b
Rafael Correa <rafael.correa@yamiassu.com.br>
2026-04-17 00:52:27 -0300

    fix: improve error logging in errorHandler middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,6 +3,8 @@ function errorHandler(err, req, res, next) {
   const statusCode = err.statusCode || (dbUnavailable ? 503 : 500);
   const message = err.message || (dbUnavailable ? 'Banco de dados indisponivel no momento.' : 'Erro interno no servidor');
 
+  console.error(err);
+
   res.status(statusCode).json({
     error: message
   });
